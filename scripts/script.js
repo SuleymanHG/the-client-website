9b4ab5b0992768b2e4e592eeebd8123452022e8b
SuleymanHuzeyfe <suleyman.gokgul66@outlook.com>
2024-10-10 09:28:17 +0200

    v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,4 @@
-// JS
+document.getElementById('hamburger').addEventListener('click', function() {
+    var navbarList = document.getElementById('navbar_list');
+    navbarList.classList.toggle('show');
+});
